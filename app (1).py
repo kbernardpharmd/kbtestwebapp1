606ebcce598588ceeb484fc67ad8eba5e66f27e1
kbernardpharmd <kimberly.bernardic@gmail.com>
2024-10-05 11:20:16 -0500

    Add files via upload

--- app (1).py
+++ app (1).py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import joblib
 from utils import preprocessor
+
 def run():
-    model = joblib.load(open('model.joblib', 'rb'))
+    model = ???
+
     st.title("Sentiment Analysis")
     st.text("Basic app to detect the sentiment of text.")
     st.text("")
@@ -11,12 +13,13 @@ def run():
     st.text("")
     predicted_sentiment = ""
     if st.button("Predict"):
-        predicted_sentiment = model.predict(pd.Series(userinput))[0]
+        predicted_sentiment = ???
         if predicted_sentiment == 1:
             output = 'positive 👍'
         else:
             output = 'negative 👎'
         sentiment=f'Predicted sentiment of "{userinput}" is {output}.'
-        st.success(sentiment)
+        st.success(???)
+
 if __name__ == "__main__":
     run()
